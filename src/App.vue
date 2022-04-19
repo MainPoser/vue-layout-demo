e68252baa36628c84087a7571d0a8a8456d079ee
tianyao <ty18710388929@163.com>
2022-04-19 17:18:23 +0800

    添加 添加基础项目布局

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div style="height: 100%">
+    <el-container style="height: 100%">
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+      <el-footer></el-footer>
+    </el-container>
+  </div>
+
+</template>
+
+<style lang="less">
+
+html,body,#app{
+  margin: 0;
+  padding: 0;
+  height:100%;
+}
+
+.el-header {
+  background: #42b983;
+}
+
+.el-aside {
+  background: blue;
+}
+
+.el-main {
+  background: darkred;
+}
+
+.el-footer {
+  background: black;
+}
+</style>
