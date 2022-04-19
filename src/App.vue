89e26503acb6e1367626abbfd90c25ea7932fc1b
tianyao <ty18710388929@163.com>
2022-04-19 17:42:02 +0800

    修改 基本简单布局

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,22 @@
 <template>
-  <div style="height: 100%">
-    <el-container style="height: 100%">
-      <el-header>Header</el-header>
-      <el-container>
+  <el-container style="height: 100%">
+    <el-header>Header</el-header>
+    <el-main>
+      <el-container style="height: 100%">
         <el-aside width="200px">Aside</el-aside>
         <el-main>Main</el-main>
       </el-container>
-      <el-footer></el-footer>
-    </el-container>
-  </div>
-
+    </el-main>
+    <el-footer></el-footer>
+  </el-container>
 </template>
 
 <style lang="less">
 
-html,body,#app{
+html, body, #app {
   margin: 0;
   padding: 0;
-  height:100%;
+  height: 100%;
 }
 
 .el-header {
